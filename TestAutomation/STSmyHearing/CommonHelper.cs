4d624a002021fc0cf0129427f3008d0f79a2bd32
Manish <maneesh.learning@gmaill.com>
2017-08-24 22:43:51 +0530

    NU Commit

--- TestAutomation/STSmyHearing/CommonHelper.cs
+++ TestAutomation/STSmyHearing/CommonHelper.cs
@@ -59,7 +59,7 @@ namespace STSmyHearing
 		public void ValidateActionBarTitle(string title)
 		{
 			if(KeywordImplementation.UIObject(By.Id("action_bar_title"),TimeSpan.FromSeconds(5)).Text.Equals(title))
-				Logger.logSuccess(title+" has found");
+				 Logger.logSuccess(title+" has found");
 			else
 				Logger.logFailure(title+" has not found");
 		}
